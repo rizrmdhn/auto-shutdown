6faa1b7375d9e563c84ca1a87a504b87277da02e
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-04-03 09:11:21 +0800

    feat(web): implement routing and update main entry point
    - set up a Router instance using @tanstack/react-router
    - update index.html to use <div id="app"></div>
    - modify main.tsx to render RouterProvider
    - enhance index.tsx to include a button with toast notification

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,30 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import {
+  RouterProvider,
+  createMemoryHistory,
+  createRouter,
+} from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  history: createMemoryHistory({ initialEntries: ["/"] }),
+  defaultPreload: "intent",
+  scrollRestoration: true,
+});
+
+// Register things for typesafety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById("app")!;
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<RouterProvider router={router} />);
+}
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,9 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button";
+import { globalSuccessToast } from "@/lib/toast";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return (
+    <div>
+      <Button onClick={() => globalSuccessToast("hello there this is toast")}>
+        Go to about
+      </Button>
+    </div>
+  );
 }
